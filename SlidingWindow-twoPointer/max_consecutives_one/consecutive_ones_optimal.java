4644bf749af531aa2f2babec2e57d3bba299d5ee
Abdul Rahman <abdularsf147@gmail.com>
2026-02-18 00:16:59 +0530

    consecutive_ones_optimal

---
+++ SlidingWindow-twoPointer/max_consecutives_one/consecutive_ones_optimal.java
@@ -0,0 +1,34 @@
+public class consecutive_ones_optimal {
+    public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int maxLen = 0;
+        int zeros = 0;
+        int left = 0;
+        int right = 0;
+        while (right < n) {
+            if (nums[right] == 0) {
+                zeros++;
+            }
+            if (zeros > k) {
+                if (nums[left] == 0) {
+                    zeros--;
+                }
+                left++;
+            }
+            if (zeros <= k) {
+                int len = right - left + 1;
+                maxLen = Math.max(maxLen, len);
+            }
+            right++;
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        consecutive_ones_optimal sol = new consecutive_ones_optimal();
+        int[] input = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
+        int k = 2;
+        int length = sol.longestOnes(input, k);
+        System.out.println("Length of longest substring with at most " + k + " zeros: " + length);
+    }
+}
