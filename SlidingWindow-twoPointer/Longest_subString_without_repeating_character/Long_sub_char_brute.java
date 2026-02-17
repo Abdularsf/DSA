160c4e9b3743331ad97ea0d13bea64d8a1d3822e
Abdul Rahman <abdularsf147@gmail.com>
2026-02-17 22:43:37 +0530

    Long_sub_brute

--- SlidingWindow-twoPointer/Longest_subString_without_repeating_character/Long_sub_char_brute.java
+++ SlidingWindow-twoPointer/Longest_subString_without_repeating_character/Long_sub_char_brute.java
@@ -1,3 +1,24 @@
 public class Long_sub_char_brute {
+    public int longestNonRepeatingSubstring(String s) {
+        int n = s.length();
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            int[] hash = new int[256];
+            for (int j = i; j < n; j++) {
+                if (hash[s.charAt(j)] == 1)
+                    break;
+                int length = j - i + 1;
+                maxLength = Math.max(maxLength, length);
+                hash[s.charAt(j)] = 1;
+            }
+        }
+        return maxLength;
+    }
 
+    public static void main(String[] args) {
+        String input = "cadbzabcd";
+        Long_sub_char_brute sol = new Long_sub_char_brute();
+        int length = sol.longestNonRepeatingSubstring(input);
+        System.out.println("Length of longest substring without repeating characters: " + length);
+    }
 }
