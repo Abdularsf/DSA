8e66434f3b7b560c74c421538de686bb710e0880
Abdul Rahman <abdularsf147@gmail.com>
2026-02-17 22:46:55 +0530

    Long_sub_optimal

--- SlidingWindow-twoPointer/Longest_subString_without_repeating_character/Long_sub_char_optimal.java
+++ SlidingWindow-twoPointer/Longest_subString_without_repeating_character/Long_sub_char_optimal.java
@@ -1,4 +1,31 @@
+import java.util.Arrays;
 
 public class Long_sub_char_optimal {
+    public int longestNonRepeatingSubstring(String s) {
+        int[] hash = new int[256];
+        Arrays.fill(hash, -1);
+        int n = s.length();
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+        while (right < n) {
+            if (hash[s.charAt(right)] != -1) {
+                if (hash[s.charAt(right)] >= left) {
+                    left = hash[s.charAt(right)] + 1;
+                }
+            }
+            int length = right - left + 1;
+            maxLength = Math.max(length, maxLength);
+            hash[s.charAt(right)] = right;
+            right++;
+        }
+        return maxLength;
+    }
 
+    public static void main(String[] args) {
+        String input = "cadbzabcd";
+        Long_sub_char_optimal sol = new Long_sub_char_optimal();
+        int length = sol.longestNonRepeatingSubstring(input);
+        System.out.println("Length of longest substring without repeating characters: " + length);
+    }
 }
