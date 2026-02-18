1ffc2a74d18645393a7e70e1eaf07674c15f8f08
Abdul Rahman <abdularsf147@gmail.com>
2026-02-18 14:47:29 +0530

    Fruit_basket_optimal

---
+++ SlidingWindow-twoPointer/FruitIntoBasket/Fruit_Basket_optimal.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class Fruit_Basket_optimal {
+    public int totalFruits(int[] fruits) {
+        int n = fruits.length;
+        int maxLen = 0;
+        int left = 0;
+        int right = 0;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        while (right < n) {
+            mpp.put(fruits[right], mpp.getOrDefault(fruits[right], 0) + 1);
+            if (mpp.size() > 2) {
+                mpp.put(fruits[left], mpp.get(fruits[left]) - 1);
+                if (mpp.get(fruits[left]) == 0) {
+                    mpp.remove(fruits[left]);
+                }
+                left++;
+            }
+            if (mpp.size() <= 2) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            right++;
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] input = { 3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4 };
+        Fruit_Basket_optimal sol = new Fruit_Basket_optimal();
+        int length = sol.totalFruits(input);
+        System.out.println("Maximum fruits in the basket is: " + length);
+    }
+}
