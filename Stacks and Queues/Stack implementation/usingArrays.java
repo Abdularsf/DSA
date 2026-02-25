43790e98ae29fbdc29de6e8e589c954649ef6de6
Abdul Rahman <abdularsf147@gmail.com>
2026-02-25 16:10:48 +0530

    stack using Arrays

---
+++ Stacks and Queues/Stack implementation/usingArrays.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class usingArrays {
+    private int[] stackArray;
+    // private int capacity;
+    private int top = -1;
+
+    public usingArrays() {
+        stackArray = new int[10];
+    }
+
+    public usingArrays(int size) {
+        stackArray = new int[size];
+    }
+
+    public void push(int x) {
+        if (top == stackArray.length - 1) {
+            System.out.println("Stack Overflow");
+            return;
+        }
+        top++;
+        stackArray[top] = x;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+        return stackArray[top--];
+    }
+
+    public int top() {
+        return stackArray[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public static void main(String[] args) {
+        usingArrays stack = new usingArrays();
+        List<String> commands = Arrays.asList("ArrayStack", "push", "push", "top", "pop", "isEmpty");
+        List<List<Integer>> inputs = Arrays.asList(Arrays.asList(), Arrays.asList(5), Arrays.asList(10),
+                Arrays.asList(), Arrays.asList(), Arrays.asList());
+        for (int i = 0; i < commands.size(); ++i) {
+            switch (commands.get(i)) {
+                case "push":
+                    stack.push(inputs.get(i).get(0));
+                    System.out.print("null ");
+                    break;
+                case "pop":
+                    System.out.print(stack.pop() + " ");
+                    break;
+                case "top":
+                    System.out.print(stack.top() + " ");
+                    break;
+                case "isEmpty":
+                    System.out.print((stack.isEmpty() ? "true" : "false") + " ");
+                    break;
+                case "ArrayStack":
+                    System.out.print("null ");
+                    break;
+            }
+        }
+    }
+
+}
