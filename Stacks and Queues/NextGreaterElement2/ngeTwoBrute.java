cffb7d1f3578107392d1ed41f9e1b4901e7cf205
Abdul Rahman <abdularsf147@gmail.com>
2026-02-28 23:34:13 +0530

    ngeTwoOptimal and changes

--- Stacks and Queues/NextGreaterElement2/ngeTwoBrute.java
+++ Stacks and Queues/NextGreaterElement2/ngeTwoBrute.java
@@ -1,36 +1,36 @@
-import java.util.Stack;
+import java.util.Arrays;
 
 public class ngeTwoBrute {
+
     public int[] nextGreaterElements(int[] arr) {
         int n = arr.length;
         int[] nge = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && st.peek() <= arr[i % n]) {
-                st.pop();
+
+        Arrays.fill(nge, -1);
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < i + n; j++) {
+
+                int index = j % n;
+
+                if (arr[index] > arr[i]) {
+                    nge[i] = arr[index];
+                    break;
+                }
             }
-            if (i < n) {
-                if (st.isEmpty())
-                    nge[i] = -1;
-                else
-                    nge[i] = st.peek();
-            }
-            st.push(arr[i % n]);
         }
         return nge;
     }
 
     public static void main(String[] args) {
-        int n = 6;
+
         int[] arr = { 5, 7, 1, 7, 6, 0 };
 
         ngeTwoBrute sol = new ngeTwoBrute();
 
         int[] ans = sol.nextGreaterElements(arr);
 
-        System.out.println("The next greater elements are: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(ans[i] + " ");
-        }
+        System.out.println(Arrays.toString(ans));
     }
 }
