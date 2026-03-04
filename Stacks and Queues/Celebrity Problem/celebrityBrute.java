a4304ddf8e158c1c3251627a8131d63443bc0ab8
Abdul Rahman <abdularsf147@gmail.com>
2026-03-04 22:02:43 +0530

    celebrityBrute

---
+++ Stacks and Queues/Celebrity Problem/celebrityBrute.java
@@ -0,0 +1,46 @@
+
+public class celebrityBrute {
+    public int celebrity(int[][] M) {
+        int n = M.length;
+        int m = M[0].length;
+        int[] knowMe = new int[n];
+        int[] iknow = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (M[i][j] == 1) {
+                    knowMe[j]++;
+                    iknow[i]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (knowMe[i] == n - 1 && iknow[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] M = {
+                { 0, 1, 1, 0 },
+                { 0, 0, 0, 0 },
+                { 1, 1, 0, 0 },
+                { 0, 1, 1, 0 }
+        };
+
+        /*
+         * Creating an instance of
+         * Solution class
+         */
+        celebrityBrute sol = new celebrityBrute();
+
+        // Function call to find the index of celebrity
+        int ans = sol.celebrity(M);
+
+        System.out.println("The index of celebrity is: " + ans);
+    }
+
+}
