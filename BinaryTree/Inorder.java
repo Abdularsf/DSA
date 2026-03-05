d2f0546f92a4e5a942d732d85c2d44d1d7dd6fd8
Abdul Rahman <abdularsf147@gmail.com>
2026-03-05 22:22:11 +0530

    inorder

--- BinaryTree/Inorder.java
+++ BinaryTree/Inorder.java
@@ -1,5 +1,55 @@
 package BinaryTree;
 
-public class Inorder {
+import java.util.ArrayList;
+import java.util.List;
 
+// import javax.swing.tree.TreeNode;
+
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        data = val;
+        left = null;
+        right = null;
+    }
+
+}
+
+public class Inorder {
+    private void helper(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
+
+        helper(root.left, list);
+        list.add(root.data);
+        helper(root.right, list);
+    }
+
+    public List<Integer> inorder(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        helper(root, list);
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        Inorder sol = new Inorder();
+
+        List<Integer> result = sol.inorder(root);
+
+        System.out.print("Inorder Traversal: ");
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
 }
