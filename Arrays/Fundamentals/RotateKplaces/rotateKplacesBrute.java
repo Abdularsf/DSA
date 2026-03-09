8580d791d3f4837bdea7ae92c044e88e1d697cf5
Abdul Rahman <abdularsf147@gmail.com>
2026-03-09 22:59:54 +0530

    rotateKplacesBrut

---
+++ Arrays/Fundamentals/RotateKplaces/rotateKplacesBrute.java
@@ -0,0 +1,50 @@
+package Arrays.Fundamentals.RotateKplaces;
+
+public class rotateKplacesBrute {
+
+    public void rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        int[] temp = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[i];
+        }
+
+        for (int i = k; i < n; i++) {
+            nums[i - k] = nums[i];
+        }
+
+        for (int i = 0; i < k; i++) {
+            nums[n - k + i] = temp[i];
+        }
+    }
+
+    public static void printArray(int[] nums) {
+        for (int val : nums) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6 };
+        int k = 2;
+
+        System.out.println("Initial array: ");
+        printArray(nums);
+
+        // Create an instance of the Solution class
+        rotateKplacesBrute sol = new rotateKplacesBrute();
+
+        /*
+         * Function call to rotate the
+         * array to the left by k places
+         */
+        sol.rotateArray(nums, k);
+
+        System.out.println("Array after rotating elements by " + k + " places: ");
+        printArray(nums);
+    }
+}
