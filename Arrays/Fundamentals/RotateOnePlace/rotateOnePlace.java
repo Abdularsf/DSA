528dd1ff1e642abb8ea60c97ba9e4e0dcc9f5b45
Abdul Rahman <abdularsf147@gmail.com>
2026-03-09 22:57:48 +0530

    rotateoneplace

---
+++ Arrays/Fundamentals/RotateOnePlace/rotateOnePlace.java
@@ -0,0 +1,24 @@
+package Arrays.Fundamentals.RotateOnePlace;
+
+public class rotateOnePlace {
+    public void rotateArrayByOne(int[] nums) {
+        int n = nums.length;
+        int temp = nums[0];
+        for (int i = 1; i < n; i++) {
+            nums[i - 1] = nums[i];
+        }
+        nums[n - 1] = temp;
+    }
+
+    public static void main(String[] args) {
+        rotateOnePlace solution = new rotateOnePlace();
+        int[] nums = { 1, 2, 3, 4, 5 };
+
+        solution.rotateArrayByOne(nums);
+
+        // Output the rotated array
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
