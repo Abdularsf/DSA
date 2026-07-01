e9135348f91fe4c8b4de3408c48eb32b18f7185a
Abdul Rahman <abdularsf147@gmail.com>
2026-07-01 13:16:05 +0530

    SRC File creation

--- Graph/ConnectedComponenets/conntectedGraph.java
+++ Graph/ConnectedComponenets/conntectedGraph.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-class Solution {
+class connectedGraph {
     // Function for BFS traversal
     private void bfs(int node, List<Integer>[] adjLs, boolean[] vis) {
         vis[node] = true;
@@ -35,6 +35,7 @@ class Solution {
     // }
     public int findNumberOfComponent(int V, List<List<Integer>> edges) {
         int E = edges.size();
+        @SuppressWarnings("unchecked")
         List<Integer>[] adjLs = new ArrayList[V];
         for (int i = 0; i < V; i++) {
             adjLs[i] = new ArrayList<>();
@@ -55,5 +56,24 @@ class Solution {
 
         // Return the count
         return cnt;
+    }
+
+    public static void main(String[] args) {
+
+        int V = 7;
+
+        List<List<Integer>> edges = new ArrayList<>();
+
+        edges.add(List.of(0, 1));
+        edges.add(List.of(0, 2));
+        edges.add(List.of(1, 2));
+        edges.add(List.of(3, 4));
+        edges.add(List.of(5, 6));
+
+        connectedGraph obj = new connectedGraph();
+
+        int components = obj.findNumberOfComponent(V, edges);
+
+        System.out.println("Number of Connected Components = " + components);
     }
 }
