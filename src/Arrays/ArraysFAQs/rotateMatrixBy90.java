5388b93fd637c9d68440cad46bd6171e369a7d16
Abdul Rahman <abdularsf147@gmail.com>
2026-07-28 23:40:01 +0530

    roate by 90

---
+++ src/Arrays/ArraysFAQs/rotateMatrixBy90.java
@@ -0,0 +1,26 @@
+package src.Arrays.ArraysFAQs;
+
+public class rotateMatrixBy90 {
+    public void rotateMatrix(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+
+                // Swap elements symmetrically
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+
+            }
+        }
+    }
+}
